37751d5f578007da9d3440a5669babaa2d2e6a9d
Ghabriel <ghabriel.nunes@gmail.com>
2019-06-11 19:47:28 -0300

    Basic rendering code

--- src/constants.hpp
+++ src/constants.hpp
@@ -9,7 +9,7 @@ namespace constants {
     constexpr float BALL_MIN_SQUARED_VELOCITY = 250000;
     constexpr float BALL_MAX_SQUARED_VELOCITY = 320000;
 
-    constexpr float PADDLE_WIDTH = 200;
+    constexpr float PADDLE_WIDTH = 150;
     constexpr float PADDLE_HEIGHT = 20;
     constexpr float PADDLE_BORDER_DISTANCE = 20;
 
--- src/systems/rendering-system.hpp
+++ src/systems/rendering-system.hpp
@@ -4,5 +4,35 @@
 #include "../engine-glue/ecs.hpp"
 
 void useRenderingSystem(ecs::ComponentManager& world, sf::RenderWindow& window) {
-    // TODO
+    world.findAll<Visible>()
+        .join<Circle>()
+        .join<Position>()
+        .forEach(
+            [&window](const Circle& circle, const Position& pos) {
+                sf::CircleShape c(circle.radius);
+                c.setFillColor(sf::Color::White);
+                c.setPosition({
+                    pos.x - circle.radius,
+                    pos.y - circle.radius
+                });
+
+                window.draw(c);
+            }
+        );
+
+    world.findAll<Visible>()
+        .join<Rectangle>()
+        .join<Position>()
+        .forEach(
+            [&window](const Rectangle& rect, const Position& pos) {
+                sf::RectangleShape r({ rect.width, rect.height });
+                r.setFillColor(sf::Color::White);
+                r.setPosition({
+                    pos.x - rect.width / 2,
+                    pos.y - rect.height / 2
+                });
+
+                window.draw(r);
+            }
+        );
 }
