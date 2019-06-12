ce4fb077975a197a2126e9419f9805b32286d4de
Ghabriel <ghabriel.nunes@gmail.com>
2019-06-12 20:28:09 -0300

    Implemented paddle movement in the "waiting state"

--- src/states/WaitingState.hpp
+++ src/states/WaitingState.hpp
@@ -2,7 +2,9 @@
 
 #include "../engine-glue/ecs.hpp"
 #include "../engine/state-management/include.hpp"
+#include "../systems/input-system/include.hpp"
 #include "../systems/level-loading-system/include.hpp"
+#include "../systems/movement-system/include.hpp"
 #include "../systems/rendering-system/include.hpp"
 
 class WaitingState : public state::State {
@@ -14,12 +16,27 @@ class WaitingState : public state::State {
 
     virtual void onEnter() override {
         useLevelLoadingSystem(world);
+
+        ecs::Entity ball = world.unique<Ball>();
+        world.addComponent<Input>(ball, { });
+    }
+
+    virtual void onExit() override {
+        ecs::Entity ball = world.unique<Ball>();
+        world.removeComponent<Input>(ball);
     }
 
     virtual void update(const sf::Time& elapsedTime) override {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
             stateMachine.pushState("running");
+            return;
         }
+
+        unsigned elapsedTimeMicro = elapsedTime.asMicroseconds();
+        float normalizedElapsedTime = elapsedTimeMicro / 1000000.0;
+
+        useInputSystem(world);
+        useMovementSystem(world, normalizedElapsedTime);
     }
 
     virtual void render(sf::RenderWindow& window) override {
