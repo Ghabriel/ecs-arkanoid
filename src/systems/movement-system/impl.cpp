6c8d70637553efab64915efee9bd37468bf36fd6
Ghabriel <ghabriel.nunes@gmail.com>
2019-06-14 10:24:58 -0300

    Added collision handling to the Movement System

--- src/systems/movement-system/impl.cpp
+++ src/systems/movement-system/impl.cpp
@@ -1,6 +1,26 @@
 #include "include.hpp"
 
+#include <algorithm>
+#include "../../constants.hpp"
+
+#include <iostream>
+
+struct CircleData {
+    const Circle& body;
+    const Position& position;
+};
+
+struct RectangleData {
+    const Rectangle& body;
+    const Position& position;
+};
+
+void resolveCollisions(ecs::ComponentManager&, float);
+bool collides(const CircleData&, const RectangleData&);
+
 void useMovementSystem(ecs::ComponentManager& world, float elapsedTime) {
+    resolveCollisions(world, elapsedTime);
+
     world.findAll<Position>()
         .join<Velocity>()
         .forEach(
@@ -9,4 +29,50 @@ void useMovementSystem(ecs::ComponentManager& world, float elapsedTime) {
                 pos.y += v.y * elapsedTime;
             }
         );
+}
+
+void resolveCollisions(ecs::ComponentManager& world, float elapsedTime) {
+    world.findAll<Ball>()
+        .join<Circle>()
+        .join<Position>()
+        .join<Velocity>()
+        .forEach([&world, elapsedTime](const Circle& c, const Position& ballPos, Velocity& v) {
+            Velocity velocity { v.x * elapsedTime, v.y * elapsedTime };
+            Position nextPositionX { ballPos.x + velocity.x, ballPos.y };
+            Position nextPositionY { ballPos.x, ballPos.y + velocity.y };
+
+            world.findAll<Rectangle>()
+                .join<Position>()
+                .forEach([&](const Rectangle& r, const Position& rectPos) {
+                    RectangleData rectangle { r, rectPos };
+
+                    if (collides(CircleData { c, nextPositionX }, rectangle)) {
+                        v.x *= -1;
+                    }
+
+                    if (collides(CircleData { c, nextPositionY }, rectangle)) {
+                        v.y *= -1;
+                    }
+                });
+            }
+        );
+}
+
+bool collides(const CircleData& c, const RectangleData& r) {
+    const auto& [circle, circlePos] = c;
+    const auto& [rectangle, rectPos] = r;
+    //{}
+
+    float rectLeftX = rectPos.x - rectangle.width / 2;
+    float rectRightX = rectPos.x + rectangle.width / 2;
+    float rectTopY = rectPos.y - rectangle.height / 2;
+    float rectBottomY = rectPos.y + rectangle.height / 2;
+
+    float closestX = std::clamp(circlePos.x, rectLeftX, rectRightX);
+    float closestY = std::clamp(circlePos.y, rectTopY, rectBottomY);
+
+    float dx = circlePos.x - closestX;
+    float dy = circlePos.y - closestY;
+
+    return (dx * dx) + (dy * dy) < (circle.radius * circle.radius);
 }
