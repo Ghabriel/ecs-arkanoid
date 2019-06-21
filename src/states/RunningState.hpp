e4fe8ef491db0ba4bfc99494736ac4f9eccbd363
Ghabriel <ghabriel.nunes@gmail.com>
2019-06-21 17:09:02 -0300

    Generalized collision listening

--- src/states/RunningState.hpp
+++ src/states/RunningState.hpp
@@ -61,36 +61,17 @@ class RunningState : public state::EffectState {
     }
 
     void listenToCollisions() {
-        listenToBallPaddleCollisions();
-        listenToBallBrickCollisions();
-        listenToBallWallCollisions();
-        listenToPaddlePowerUpCollisions();
-        listenToPaddleWallCollisions();
+        listenToCollisions<Ball, Brick>();
+        listenToCollisions<Ball, Paddle>();
+        listenToCollisions<Ball, Wall>();
+        listenToCollisions<Paddle, PowerUp>();
+        listenToCollisions<Paddle, Wall>();
     }
 
-    void listenToBallPaddleCollisions() {
-        auto callback = partialApply(useCollisionSystem<Ball, Paddle>, world);
-        useToggleComponentEffect(listenerId, CollisionListener<Ball, Paddle> { callback });
-    }
-
-    void listenToBallBrickCollisions() {
-        auto callback = partialApply(useCollisionSystem<Ball, Brick>, world);
-        useToggleComponentEffect(listenerId, CollisionListener<Ball, Brick> { callback });
-    }
-
-    void listenToBallWallCollisions() {
-        auto callback = partialApply(useCollisionSystem<Ball, Wall>, world);
-        useToggleComponentEffect(listenerId, CollisionListener<Ball, Wall> { callback });
-    }
-
-    void listenToPaddlePowerUpCollisions() {
-        auto callback = partialApply(useCollisionSystem<Paddle, PowerUp>, world);
-        useToggleComponentEffect(listenerId, CollisionListener<Paddle, PowerUp> { callback });
-    }
-
-    void listenToPaddleWallCollisions() {
-        auto callback = partialApply(useCollisionSystem<Paddle, Wall>, world);
-        useToggleComponentEffect(listenerId, CollisionListener<Paddle, Wall> { callback });
+    template<typename T, typename U>
+    void listenToCollisions() {
+        auto callback = partialApply(useCollisionSystem<T, U>, world);
+        useToggleComponentEffect(listenerId, CollisionListener<T, U> { callback });
     }
 
     void listenToGameOver() {
